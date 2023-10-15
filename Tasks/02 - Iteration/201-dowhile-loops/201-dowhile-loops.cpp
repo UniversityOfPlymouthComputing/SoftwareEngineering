c59ec86e15483400069a3475b7d8d502ef31c4bf
Tavizee <147332225+Tavizee@users.noreply.github.com>
2023-10-15 22:54:17 +0100

    201 do while
    interesting

--- Tasks/02 - Iteration/201-dowhile-loops/201-dowhile-loops.cpp
+++ Tasks/02 - Iteration/201-dowhile-loops/201-dowhile-loops.cpp
@@ -15,5 +15,21 @@ int main()
     } while (i < 5);            //Repeat if condition is met
 
     printf("Finished: i = %d\n", i);
+ }
+int main()
+{
+    int i = 0;
+
+    printf("Start while loop\n");
+
+    do
+    {
+        printf("i =%d\n", i);
+        i = i + 2;
+    } while (i < 6);
+
+    printf("Finish: i = %d\n", i);
 }
+
+
 
