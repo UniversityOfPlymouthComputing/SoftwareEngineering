61cf73c8d6d738f8cd9635fd6105481a0cecfd3a
Tavizee <147332225+Tavizee@users.noreply.github.com>
2023-10-15 19:12:27 +0100

    103 practise

--- Tasks/01 - Variables and Arithmetic/101-WritingToTheConsole/103-WritingToTheConsole.cpp
+++ Tasks/01 - Variables and Arithmetic/101-WritingToTheConsole/103-WritingToTheConsole.cpp
@@ -3,35 +3,15 @@
 int main()
 {
     // ************************ Using puts ******************************** 
-    puts("Hello World!");   //Write Hello World to the terminal
+    puts("Hello World!\n");   //Write Hello World to the terminal
 
-
-    // ************************ Using printf ******************************* 
+    printf("Hello World!\n");
+        printf("Hello Everyone. We are using C\nHow are things?\n");
     printf("Hello Everyone. We are using C\nHow are things?\n");
-    // TASK (a) - remove \n from the string above - what does this change? 
-    
-    // ******************* Using printf placeholders *********************** 
     int age = 23;
     printf("My age is %d\n", age);
-
-    // Some other placeholders include %c for character and %f for floating point
-    float radius = 10.0f;
-    float area = 3.415926 * radius * radius;
-
-    // TASK(b) - Use printf to display the value of the variable area. Look at the lecture slides and find out how to limit this to 3 decimal places
-    // Write solution here
-    
-    //
-
-    // You can output complex strings to the terminal
-    int p = 3;
-    int q = 4;
-    printf("p=%d, q=%d\n", p, q);
-
-    // TASK(c) - using printf, display the radius and the area using a single printf statement
-    // Write solution here
-
-    //
+    int age = 35;
+    printf("age=%d,age\n"); 
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
