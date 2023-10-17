98c695bbfa80819ab9fe7a48b742ff2130a4fef6
Tavizee <147332225+Tavizee@users.noreply.github.com>
2023-10-17 13:17:13 +0100

    Update 203-dowhile-getchar.cpp

--- Tasks/02 - Iteration/203-dowhile-getchar/203-dowhile-getchar.cpp
+++ Tasks/02 - Iteration/203-dowhile-getchar/203-dowhile-getchar.cpp
@@ -1,21 +1,22 @@
 #include <stdio.h>
 
-int main()
+
+int main ()
 {
+
     puts("Press a letter or number, then press return");
     puts("Press q then return to quit");
 
-    char userEnteredCharacter;
-    char returnKey;
+    char userenteredcharacter;
+    char returnkey;
+do
+{
+    userenteredcharacter = getchar();
+    returnkey = getchar();
+    printf("you type %c\n", userentercharacter);
+    printf("you also pressed returnkey (userenteredcharacter %d)\n", returnkey);
+}
+while ((userentercharacter != 'q') && (userentercharacter != 'Q'));
 
-    do
-    {
-        userEnteredCharacter = getchar();   //ASCII character
-        returnKey = getchar();              //Return key
-        printf("You typed %c\n", userEnteredCharacter);
-        //printf("You also pressed return (ASCII %d)\n", returnKey);    //Uncomment this
-
-    } while (userEnteredCharacter != 'q');            //Repeat if condition is met - note the != operator
-
-    puts("Finished");
+     puts("Finished");
 }
