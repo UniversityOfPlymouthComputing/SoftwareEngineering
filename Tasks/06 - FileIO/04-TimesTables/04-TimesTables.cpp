f25566d8938ca7d23a047161480155072a22f32b
Gatthub <146229893+Gatthub@users.noreply.github.com>
2023-10-26 15:00:45 +0100

    Work home

--- Tasks/06 - FileIO/04-TimesTables/04-TimesTables.cpp
+++ Tasks/06 - FileIO/04-TimesTables/04-TimesTables.cpp
@@ -5,13 +5,20 @@ using namespace std;
 
 int main()
 {
+	ofstream outputStream;
+	outputStream.open("tables.txt");
+	if (outputStream.is_open() == false) {
+		cerr << "Cannot create file" << endl;
+		return -1;
+	}
 	for (unsigned int r = 2; r <= 12; r++) {
 		for (unsigned int c = 2; c <= 12; c++) {
-			cout << r * c << "\t";
+			outputStream << r * c << "\t";
 		}
-		cout << endl;
+		outputStream << endl;
 	}
-	cout << endl;
+	outputStream << endl;
 
+	outputStream.close();
 	return 0;
 }
